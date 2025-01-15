547828ee3568bc081a38c133ba3f41d209ae73e5
Emilio Sosa <emiliossosa85@gmail.com>
2025-01-15 01:26:36 -0500

    Add resources and content for the homepage

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,5 @@
 var year = new Date().getFullYear()
 var lastModified = new Date(document.lastModified)
 
-document.querySelector("#currentyear").innerHTML = `Last Modified: ${lastModified}`
-document.querySelector("#copyright").innerHTML = `${year}`;
+document.querySelector("#lastModified").innerHTML = `Last Modified: ${lastModified}`
+document.querySelector("#currentyear").innerHTML = `&#169;${year}`;
