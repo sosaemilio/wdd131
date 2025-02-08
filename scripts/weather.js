849478781d625d5b9920e47f673531c2defc0db3
Emilio Sosa <emiliossosa85@gmail.com>
2025-02-08 18:55:26 -0500

    Windchill feature

---
+++ scripts/weather.js
@@ -0,0 +1,10 @@
+//DOM Functions
+const tempText = document.getElementById("temperature");
+const conditionsText = document.getElementById("conditions");
+const windText = document.getElementById("wind");
+const windChillText = document.getElementById("wind-chill");
+
+tempText.innerHTML = 24;
+conditionsText.innerHTML = "Partly cloudy";
+windText.innerHTML = "8 Km/H";
+windChillText = 7.24
