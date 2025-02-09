699e8794ad3258d68dc419739210fd4f05536ab3
Emilio Sosa <emiliossosa85@gmail.com>
2025-02-08 19:18:08 -0500

    Fix weather formula

--- scripts/weather.js
+++ scripts/weather.js
@@ -4,7 +4,16 @@ const conditionsText = document.getElementById("conditions");
 const windText = document.getElementById("wind");
 const windChillText = document.getElementById("wind-chill");
 
-tempText.innerHTML = 24;
+let temperature = 24;
+let wind = 8;
+
+
+function calculateWindChill(temperature, wind) {
+    let windChill = 13.12 + 0.6215 * temperature - 11.37 * wind ** 0.16 + 0.3965 * temperature * wind ** 0.16;
+    return windChill;
+}
+
+tempText.innerHTML = 24 + "C";
 conditionsText.innerHTML = "Partly cloudy";
-windText.innerHTML = "8 Km/H";
-windChillText = 7.24
+windText.innerHTML = wind + " Km/H";
+windChillText.innerHTML = calculateWindChill(temperature, wind);
