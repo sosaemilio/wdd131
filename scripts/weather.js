d1184a42afa60e5b12677288bc0448f8d4a65547
Emilio Sosa <emiliossosa85@gmail.com>
2025-02-08 19:24:06 -0500

    Fix Weather

--- scripts/weather.js
+++ scripts/weather.js
@@ -4,16 +4,23 @@ const conditionsText = document.getElementById("conditions");
 const windText = document.getElementById("wind");
 const windChillText = document.getElementById("wind-chill");
 
-let temperature = 24;
-let wind = 8;
-
-
+// Functions
 function calculateWindChill(temperature, wind) {
     let windChill = 13.12 + 0.6215 * temperature - 11.37 * wind ** 0.16 + 0.3965 * temperature * wind ** 0.16;
     return windChill;
 }
 
-tempText.innerHTML = 24 + "C";
+// Variables
+
+let temperature = 9;
+let wind = 8;
+
+if (temperature < 10 && wind > 4.8) {
+    let windChill = calculateWindChill(temperature, wind);
+    windChillText.innerHTML = Math.round(windChill * 100) / 100;;
+}
+
+// Apend Text
+tempText.innerHTML = temperature + " C";
 conditionsText.innerHTML = "Partly cloudy";
 windText.innerHTML = wind + " Km/H";
-windChillText.innerHTML = calculateWindChill(temperature, wind);
