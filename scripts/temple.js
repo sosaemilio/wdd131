5bda385ee56344c902b02b57cfc08c1af67e502a
Emilio Sosa <emiliossosa85@gmail.com>
2025-02-15 20:43:14 -0500

    menu

--- scripts/temple.js
+++ scripts/temple.js
@@ -1,13 +1,13 @@
-const menubutton = document.querySelector("#menu-icon");
-const menuitems = document.querySelectorAll(".menu-item");
+const menuButton = document.getElementById("menu-icon");
+const navBar = document.querySelectorAll(".navBar");
 // menuitems will be a Node List of the list items
 
-menubutton.addEventListener("click", () => {
-    if (menubutton.src.includes("images/menu.svg")) {
-        menuitems.forEach((item) => item.classList.toggle("menu-item"));
-        menubutton.src = "images/close-icon.svg";
+menuButton.addEventListener("click", (e) => {
+    if (menuButton.src.includes("images/menu.svg")) {
+        navBar.style.display = 'flex';
+        menuButton.src = "images/close-icon.svg";
     } else {
         menuitems.forEach((item) => item.classList.toggle("menu-item"));
-        menubutton.src = "images/menu.svg"
+        menuButton.src = "images/menu.svg"
     }
 });
