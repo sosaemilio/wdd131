3bc6678c479803b83cd9e0ed74b32d1678010dec
Emilio Sosa <emiliossosa85@gmail.com>
2025-06-19 06:57:18 +0200

    fix menu

--- scripts/menu.js
+++ scripts/menu.js
@@ -3,7 +3,7 @@ const menuIcon = document.getElementById("menu-icon");
 menuIcon.addEventListener("click", (e) => {
     // Pick menu and display
     const menuSelector = document.querySelector(".menu");
-    if (menuIcon.src.includes("../images/menu.svg")) {
+    if (menuIcon.src.includes("images/menu.svg")) {
         // Displays the menu
         menuSelector.style.display = "flex";
         // Update the src image to the close button
@@ -12,6 +12,6 @@ menuIcon.addEventListener("click", (e) => {
         // Hides the menu
         menuSelector.style.display = "none";
         // Update the src image to the menu button
-        menuIcon.src = "./images/menu.svg"
+        menuIcon.src = "../images/menu.svg"
     }
 })
